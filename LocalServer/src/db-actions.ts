8ca2f6c158bb1dd8cb67fa38c4d21a631918bd72
Mateusz Kedzierski <mateusz.kedzierski96@gmail.com>
2024-07-06 17:34:37 +0200

    refactor: encapsulate DB interaction into repository class
    - Created a repository class to encapsulate database interactions

--- LocalServer/src/db-actions.ts
+++ LocalServer/src/db-actions.ts
@@ -1,88 +1,89 @@
-import sqlite3 from "sqlite3";
+import sqlite3, { Database } from 'sqlite3';
 
-// Open the database connection
-const db = new sqlite3.Database("../database.db");
-
-// Create a table if it doesn't exist
-db.run(`
+export class SensorDataRepository {
+  #db: Database;
+  constructor() {
+    this.#db = new sqlite3.Database('database.db');
+    this.#db.run(`
   CREATE TABLE IF NOT EXISTS data (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     sensor TEXT,
     value TEXT,
     unit TEXT,
-    time TEXT
+    timestamp TEXT
   )
 `);
+  }
+  addData(
+    sensor: string,
+    value: string,
+    unit: string,
+    timestamp: string
+  ): Promise<number> {
+    return new Promise((resolve, reject) => {
+      this.#db.run(
+        'INSERT INTO data (sensor, value, unit, timestamp) VALUES (?, ?, ?, ?)',
+        [sensor, value, unit, timestamp],
+        function (err) {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(this.lastID);
+          }
+        }
+      );
+    });
+  }
 
-export function addData(
-  sensor: string,
-  value: string,
-  unit: string,
-  time: Date
-): Promise<number> {
-  return new Promise((resolve, reject) => {
-    db.run(
-      "INSERT INTO data (sensor, value, unit, time) VALUES (?, ?, ?, ?)",
-      [sensor, value, unit, time.toISOString()],
-      function (err) {
+  getAllData(): Promise<any[]> {
+    return new Promise((resolve, reject) => {
+      this.#db.all('SELECT * FROM data', (err, rows) => {
         if (err) {
           reject(err);
         } else {
-          resolve(this.lastID);
+          resolve(rows);
         }
-      }
-    );
-  });
-}
-
-export function getAllData(): Promise<any[]> {
-  return new Promise((resolve, reject) => {
-    db.all("SELECT * FROM data", (err, rows) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(rows);
-      }
+      });
     });
-  });
-}
+  }
 
-export function updateData(
-  id: number,
-  sensor: string,
-  value: string,
-  unit: string,
-  time: Date
-): Promise<void> {
-  return new Promise((resolve, reject) => {
-    db.run(
-      "UPDATE users SET sensor = ?, value = ?, unit = ?, time = ? WHERE id = ?",
-      [sensor, value, unit, time.toISOString(), id],
-      function (err) {
+  updateData(
+    id: number,
+    sensor: string,
+    value: string,
+    unit: string,
+    timestamp: Date
+  ): Promise<void> {
+    return new Promise((resolve, reject) => {
+      this.#db.run(
+        'UPDATE data SET sensor = ?, value = ?, unit = ?, timestamp = ? WHERE id = ?',
+        [sensor, value, unit, timestamp.toISOString(), id],
+        function (err) {
+          if (err) {
+            reject(err);
+          } else {
+            resolve();
+          }
+        }
+      );
+    });
+  }
+
+  // Delete a user by ID
+  deleteData(id: number): Promise<void> {
+    return new Promise((resolve, reject) => {
+      this.#db.run('DELETE FROM data WHERE id = ?', [id], function (err) {
         if (err) {
           reject(err);
         } else {
           resolve();
         }
-      }
-    );
-  });
-}
-
-// Delete a user by ID
-export function deleteData(id: number): Promise<void> {
-  return new Promise((resolve, reject) => {
-    db.run("DELETE FROM data WHERE id = ?", [id], function (err) {
-      if (err) {
-        reject(err);
-      } else {
-        resolve();
-      }
+      });
     });
-  });
-}
+  }
 
-// Close the database connection
-export function closeDatabase(): void {
-  db.close();
+  // Close the database connection
+  closeDatabase(): void {
+    this.#db.close();
+  }
 }
