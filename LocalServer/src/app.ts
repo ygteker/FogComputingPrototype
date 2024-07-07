7c83323198696a0e72d5474dd931455b64722f24
Mateusz Kedzierski <mateusz.kedzierski96@gmail.com>
2024-07-07 21:52:42 +0200

    feat: utilize MessageQueue to send data to cloud component

--- LocalServer/src/app.ts
+++ LocalServer/src/app.ts
@@ -1,6 +1,8 @@
 import { connect } from 'mqtt';
 import WebSocket from 'ws';
+import { MessageQueue } from './message-queue';
 import { SensorDataRepository } from './sensor-data.repository';
+import SensorData from './types/data';
 import { SensorDataDto } from './types/sensor-data.dto';
 
 // MQTT config
@@ -13,6 +15,8 @@ const socketAddress = 'localhost:8080';
 const socket = new WebSocket(`ws://${socketAddress}`);
 const mqttClient = connect(`mqtt://${mqttBrokerHost}`);
 const sensorDataRepository = new SensorDataRepository();
+
+const messageQueue = new MessageQueue();
 
 mqttClient.on('connect', () => {
   console.log('Connected to the MQTT broker.');
@@ -30,14 +34,31 @@ mqttClient.on('message', (topic, payload) => {
   const incomingData: SensorDataDto = JSON.parse(payload.toString());
   sensorDataRepository
     .addData(sensor, incomingData.value, 'celsius', incomingData.timestamp)
-    .then((id) => console.log(`Stored data with id: ${id}`));
+    .then((id) => {
+      console.log(`Stored data with id: ${id}`);
+      messageQueue.add$.next({
+        id: id,
+        sensor: sensor,
+        timestamp: incomingData.timestamp,
+        value: incomingData.value,
+        unit: 'celsius',
+      });
+    });
 });
+
+messageQueue.added$.subscribe((sData) => socket.send(JSON.stringify(sData)));
 
 socket.on('open', () => {
   console.log('Connected to the server');
 
-  socket.on('message', (message) => {
-    console.log(`Received from server: ${message}`);
+  socket.on('message', (confirmation) => {
+    const parsedConfirmation = JSON.parse(confirmation.toString()) as {
+      id: number;
+    };
+    console.log(
+      `Received confirmation for data point with id: ${parsedConfirmation.id}`
+    );
+    messageQueue.delivered$.next(parsedConfirmation.id);
   });
 
   socket.on('close', () => {
--- LocalServer/src/types/data.ts
+++ LocalServer/src/types/data.ts
@@ -1,5 +1,5 @@
-export default interface Data {
-  id: string;
+export default interface SensorData {
+  id: number;
   sensor: string;
   value: string;
   unit: string;
